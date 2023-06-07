47db52ed9bda137ea06d415789125c2f62a87c74
Emvit <81639393+ErikMekiassen@users.noreply.github.com>
2023-06-07 09:46:05 +0200

    latest working version
    TODO make a small notes service

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const envPath = path.resolve(__dirname, '..//.env');
 // set opp dotenv til å finne veien til .env
 dotenv.config({ path: envPath });
 app.use(cors());
+app.use(express.json())
 app.use('/v1', routes);
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`server started on port ${process.env.SERVER_PORT}`);
